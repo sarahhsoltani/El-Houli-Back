3c4f7c6c5dcb8b0abb3a2a9d95c774fa7b65648b
sarahhsoltani <sarasoltani.simplon.enda@gmail.com>
2021-08-11 22:01:03 +0100

    partie back

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const  colors=require ('colors')
+const app = express();
+//connexion db
+connectDB();
+app.use(express.json());
+
+//routers
+const users=require("./routes/usersRouter")
+const produits=require("./routes/produitRouter")
+const comment=require ("./routes/commentRouter")
+const orders=require("./routes/orderRouter")
+//middlewares
+app.use('/api/users',users)  
+app.use('/api/produits',produits)
+app.use('/api/comments',comment)
+app.use("/api/orders",orders)
+//config server
+const port = process.env.PORT || 4000; 
+app.listen(port, () => console.log(`Server running on port ${port}`.yellow.bold));  
